3e65a530d840c03ab4b8514fc82ce8d6d1ce2ae9
mikael <mikael@localhost>
2012-01-20 15:57:50 +0100

    Adding ActionBar

--- src/no/hials/muldvarp/MainActivity.java
+++ src/no/hials/muldvarp/MainActivity.java
@@ -1,14 +1,19 @@
 package no.hials.muldvarp;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import no.hials.muldvarp.courses.CourseActivity;
+import no.hials.muldvarp.desktop.TabListener;
 
 public class MainActivity extends Activity
 {
+    private static final String DESKTOP_FRAGMENT  = "desktop";
+    private static final String SETTINGS_FRAGMENT = "settings";
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -16,15 +21,30 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button testbutton = (Button) findViewById(R.id.coursesbutton);
-        testbutton.setOnClickListener(new View.OnClickListener() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);  
 
-            public void onClick(View view) {
+        ActionBar.Tab tab = actionBar.newTab();
+        tab.setText(R.string.desktop_name)
+           .setTabListener(new TabListener<DesktopFragment>(
+           this, DESKTOP_FRAGMENT, DesktopFragment.class));
+        actionBar.addTab(tab);        
+
+        tab = actionBar.newTab();
+        tab.setText(R.string.settings_name)
+           .setTabListener(new TabListener<DesktopFragment>(
+           this, SETTINGS_FRAGMENT, DesktopFragment.class));
+        actionBar.addTab(tab);           
+        
+        /*Button testbutton = (Button) findViewById(R.id.coursesbutton);
+        testbutton.setOnClickListener(new View.OnClickListener() {
+          public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), CourseActivity.class);
                 startActivityForResult(myIntent, 0);
             }
-
-        });
+        });*/
     }
     
 }
---
+++ src/no/hials/muldvarp/SettingsFragment.java
@@ -0,0 +1,19 @@
+package no.hials.muldvarp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ *
+ * @author mikael
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
