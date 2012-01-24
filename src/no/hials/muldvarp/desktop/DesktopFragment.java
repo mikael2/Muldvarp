6580fa07d94224f335fa7625de85eac777d63d2b
mikael <mikael@localhost>
2012-01-24 14:51:11 +0100

    Story ID:23872055 Added text to icons on desktop

--- src/no/hials/muldvarp/desktop/DesktopFragment.java
+++ src/no/hials/muldvarp/desktop/DesktopFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import no.hials.muldvarp.R;
 import no.hials.muldvarp.courses.CourseActivity;
 import no.hials.muldvarp.library.LIBMainscreen;
