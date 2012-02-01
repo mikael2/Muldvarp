6e73584e5dbae99148d25759f04987e9732e5b66
Terje Wallem <nospherus01@gmail.com>
2012-02-01 02:51:52 +0100

    Fixed a lot on the Detail view including the action bar. I also changed the app icon to one more fitting to the working title.

--- src/no/hials/muldvarp/library/LIBmainAll.java
+++ src/no/hials/muldvarp/library/LIBmainAll.java
@@ -7,9 +7,7 @@ package no.hials.muldvarp.library;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.Button;
 import no.hials.muldvarp.R;
 
@@ -51,4 +49,5 @@ public class LIBmainAll extends Fragment {
             }
         });
     }
+   
 }
--- src/no/hials/muldvarp/library/LibraryDetail.java
+++ src/no/hials/muldvarp/library/LibraryDetail.java
@@ -4,16 +4,19 @@
  */
 package no.hials.muldvarp.library;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 import no.hials.muldvarp.R;
-import org.xml.sax.Parser;
 
 /**
  *
@@ -25,12 +28,30 @@ public class LibraryDetail extends Activity {
      * Called when the activity is first created.
      */
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        // ToDo add your GUI initialization code here     
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ToDo add your GUI initialization code here 
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
         setContentView(R.layout.library_detail);
         final Activity mActivity = this;
         final Button button = (Button) findViewById(R.id.readbutton);
+        TextView t1 = (TextView)findViewById(R.id.texttitle);
+        t1.setText("Dette er en beskrivende tittel");
+        TextView t2 = (TextView)findViewById(R.id.textalternatetitle);
+        t2.setText("Alternate title: How to make money");
+        TextView t3 = (TextView)findViewById(R.id.textauthor);
+        t3.setText("Author: Robert Downey Jr.");
+        TextView t4 = (TextView)findViewById(R.id.textcoauthor);
+        t4.setText("Co Author(s): Clark Kent");
+        TextView t5 = (TextView)findViewById(R.id.textpageno);
+        t5.setText("Number of pages: " + 233);
+        TextView t6 = (TextView)findViewById(R.id.textpublished);
+        t6.setText("Published: 24.03.1965");
+        TextView t7 = (TextView)findViewById(R.id.textuploaded);
+        t7.setText("Uploaded: 30.01.2012");
+        TextView t8 = (TextView)findViewById(R.id.textsummary);
+        t8.setText("Summary: The Revelation of Jesus Christ, which God gave unto him, to shew unto his servants things which must shortly come to pass; and he sent and signified it by his angel unto his servant John: Who bare record of the word of God, and of the testimony of Jesus Christ, and of all things that he saw. Blessed is he that readeth, and they that hear the words of this prophecy, and keep those things which are written therein: for the time is at hand.");
          button.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
                 String url = "http://www.hials.no/nor/content/download/49530/977307/file/PB01%20ETABLERING%20AV%20NYE%20STUDIETILBUD%20TIL%20OG%20MED%2030%20STUDIEPOENG.pdf";
@@ -39,6 +60,20 @@ public class LibraryDetail extends Activity {
                 i.setDataAndType(Uri.parse(googleDocsUrl + url), "text/html");
                 startActivity(i);
              }
-         });}
+         });
+         
+         final ToggleButton togglebutton = (ToggleButton) findViewById(R.id.togglebutton);
+         togglebutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (togglebutton.isChecked()) {
+              togglebutton.setButtonDrawable(R.drawable.star_full);
+              Toast.makeText(LibraryDetail.this, "Added to favourites", Toast.LENGTH_SHORT).show();
+         } else {
+                togglebutton.setButtonDrawable(R.drawable.star_empty);
+                Toast.makeText(LibraryDetail.this, "Removed from favourites", Toast.LENGTH_SHORT).show();
+         }
+            }
+    });
     }
+}
 
