4d77b837f93de6bc223e9404740d368155806762
Kristoffer Strøm Bergset <kristoffer@bergset.no>
2012-02-02 17:43:51 +0100

    Added on click functions for CourseDetailWorkFragment list

--- src/no/hials/muldvarp/courses/CourseDetailWorkFragment.java
+++ src/no/hials/muldvarp/courses/CourseDetailWorkFragment.java
@@ -6,20 +6,15 @@ package no.hials.muldvarp.courses;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.CheckBox;
 import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.ExpandableListContextMenuInfo;
 import android.widget.TextView;
 import java.util.ArrayList;
 import no.hials.muldvarp.R;
@@ -72,116 +67,42 @@ public class CourseDetailWorkFragment extends Fragment {
         tasks.add( task );
         
         
-        //adapter = new MyExpandableListAdapter();
         TaskAdapter adapter = new TaskAdapter(this.getActivity().getApplicationContext(), groupNames, tasks);
         listview.setAdapter(adapter);
         
-        registerForContextMenu(listview);
+//        registerForContextMenu(listview);
         
         return fragmentView;
     }
     
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-        menu.setHeaderTitle("Sample menu");
-        menu.add(0, 0, 0, "Test");
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        ExpandableListContextMenuInfo info = (ExpandableListContextMenuInfo) item.getMenuInfo();
-
-        String title = ((TextView) info.targetView).getText().toString();
-
-        int type = ExpandableListView.getPackedPositionType(info.packedPosition);
-        if (type == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
-            int groupPos = ExpandableListView.getPackedPositionGroup(info.packedPosition); 
-            int childPos = ExpandableListView.getPackedPositionChild(info.packedPosition); 
-//            Toast.makeText(this, title + ": Child " + childPos + " clicked in group " + groupPos,
+//    
+//    @Override
+//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+//        menu.setHeaderTitle("Sample menu");
+//        menu.add(0, 0, 0, "Test");
+//    }
+//
+//    @Override
+//    public boolean onContextItemSelected(MenuItem item) {
+//        ExpandableListContextMenuInfo info = (ExpandableListContextMenuInfo) item.getMenuInfo();
+//
+//        String title = ((TextView) info.targetView).getText().toString();
+//
+//        int type = ExpandableListView.getPackedPositionType(info.packedPosition);
+//        if (type == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
+//            int groupPos = ExpandableListView.getPackedPositionGroup(info.packedPosition); 
+//            int childPos = ExpandableListView.getPackedPositionChild(info.packedPosition); 
+//            Toast.makeText(this.getActivity().getApplicationContext(), title + ": Child " + childPos + " clicked in group " + groupPos,
 //                    Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (type == ExpandableListView.PACKED_POSITION_TYPE_GROUP) {
-            int groupPos = ExpandableListView.getPackedPositionGroup(info.packedPosition); 
-//            Toast.makeText(this, title + ": Group " + groupPos + " clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
-        return false;
-    }
-    
-    public class MyExpandableListAdapter extends BaseExpandableListAdapter {
-        // Sample data set.  children[i] contains the children (String[]) for groups[i].
-        private String[] groups = { "Tema 1", "Tema 2", "Tema 3", "Tema 4" };
-        private String[][] children = {
-                { "Les eBook blabla", "Se video blabla", "Gjør tutorial blabla" },
-                { "Les eBook blabla", "Se video blabla", "Gjør tutorial blabla" },
-                { "Les eBook blabla", "Se video blabla", "Gjør tutorial blabla" },
-                { "Les eBook blabla", "Se video blabla", "Gjør tutorial blabla" }
-        };
-
-        public Object getChild(int groupPosition, int childPosition) {
-            return children[groupPosition][childPosition];
-        }
-
-        public long getChildId(int groupPosition, int childPosition) {
-            return childPosition;
-        }
-
-        public int getChildrenCount(int groupPosition) {
-            return children[groupPosition].length;
-        }
-
-        public TextView getGenericView() {
-            // Layout parameters for the ExpandableListView
-            AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT, 64);
-
-            TextView textView = new TextView(getActivity().getApplicationContext());
-            textView.setLayoutParams(lp);
-            // Center the text vertically
-            textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-            // Set the text starting position
-            textView.setPadding(66, 0, 0, 0);
-            return textView;
-        }
-
-        public View getChildView(int groupPosition, int childPosition, boolean isLastChild,
-                View convertView, ViewGroup parent) {
-            
-            TextView textView = getGenericView();
-            textView.setText(getChild(groupPosition, childPosition).toString());
-            return textView;
-        }
-
-        public Object getGroup(int groupPosition) {
-            return groups[groupPosition];
-        }
-
-        public int getGroupCount() {
-            return groups.length;
-        }
-
-        public long getGroupId(int groupPosition) {
-            return groupPosition;
-        }
-
-        public View getGroupView(int groupPosition, boolean isExpanded, View convertView,
-                ViewGroup parent) {
-            
-            TextView textView = getGenericView();
-            textView.setText(getGroup(groupPosition).toString());
-            return textView;
-        }
-
-        public boolean isChildSelectable(int groupPosition, int childPosition) {
-            return true;
-        }
-
-        public boolean hasStableIds() {
-            return true;
-        }
-
-    }
+//            return true;
+//        } else if (type == ExpandableListView.PACKED_POSITION_TYPE_GROUP) {
+//            int groupPos = ExpandableListView.getPackedPositionGroup(info.packedPosition); 
+//            Toast.makeText(this.getActivity().getApplicationContext(), title + ": Group " + groupPos + " clicked", Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
+//
+//        return false;
+//    }
     
     public class TaskAdapter extends BaseExpandableListAdapter {
 
@@ -213,13 +134,33 @@ public class CourseDetailWorkFragment extends Fragment {
             v = convertView;
         else
             v = inflater.inflate(R.layout.course_work_child, parent, false); 
-        Task t = (Task)getChild( groupPosition, childPosition );
+        final Task t = (Task)getChild( groupPosition, childPosition );
 		TextView name = (TextView)v.findViewById( R.id.childname );
 		if( name != null ) {
                     name.setText(t.getName());
                 }		
 		CheckBox cb = (CheckBox)v.findViewById( R.id.checkbox );
-        cb.setChecked( t.getDone() );
+                cb.setChecked(t.getDone());
+                
+                v.setOnClickListener(new OnClickListener() {
+
+                public void onClick(View v) {
+                    Intent myIntent = new Intent(v.getContext(), CoursePublicDetailActivity.class);
+                    startActivityForResult(myIntent, 0);
+                }
+            });
+                
+            cb.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {
+                    // Perform action on clicks, depending on whether it's now checked
+                    if (((CheckBox) v).isChecked()) {
+                        t.setDone(true);
+                    } else {
+                        t.setDone(false);
+                    }
+                }
+            });
+        
         return v;
     }
 
