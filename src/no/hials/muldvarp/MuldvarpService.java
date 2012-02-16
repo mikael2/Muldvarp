5200a67d27048df57434d9b11ad53f0a1a4eac1c
Kristoffer Strøm Bergset <kristoffer@bergset.no>
2012-02-16 13:58:05 +0100

    CourseActivity now uses MuldvarpService to download and cache REST data, CourseActivity reads data from cache

--- src/no/hials/muldvarp/MuldvarpService.java
+++ src/no/hials/muldvarp/MuldvarpService.java
@@ -4,24 +4,23 @@
  */
 package no.hials.muldvarp;
 
-import android.app.ProgressDialog;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Binder;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
-import com.google.gson.Gson;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URI;
-import java.util.ArrayList;
-import no.hials.muldvarp.courses.Course;
-import no.hials.muldvarp.courses.SearchResults;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -32,30 +31,15 @@ import org.apache.http.impl.client.DefaultHttpClient;
  */
 public class MuldvarpService extends Service {
     int mStartMode;       // indicates how to behave if the service is killed
-    IBinder mBinder;      // interface for clients that bind
+    // Binder given to clients
+    private final IBinder mBinder = new LocalBinder();
     boolean mAllowRebind; // indicates whether onRebind should be used
-    
-    private ServiceHandler mServiceHandler;
-    
+        
     String server = "http://master.uials.no:8080/muldvarp/";
     String course = server + "resources/course";
-    
-    ArrayList<Course> courses = new ArrayList<Course>();
-    
-    
+    String courseCache = "CourseCache";
+        
     SharedPreferences preferences;
-    
-    // Handler that receives messages from the thread
-      private final class ServiceHandler extends Handler {
-          public ServiceHandler(Looper looper) {
-              super(looper);
-          }
-          @Override
-          public void handleMessage(Message msg) {
-              
-              stopSelf(msg.arg1);
-          }
-      }
     
     @Override
     public void onCreate() {
@@ -78,17 +62,25 @@ public class MuldvarpService extends Service {
             
         }
 
-//              Message msg = mServiceHandler.obtainMessage();
-//              msg.arg1 = startId;
-//              mServiceHandler.sendMessage(msg);
-        
         return mStartMode;
     }
+    
+    /**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        public MuldvarpService getService() {
+            return MuldvarpService.this;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        // A client is binding to the service with bindService()
         return mBinder;
     }
+    
     @Override
     public boolean onUnbind(Intent intent) {
         // All clients have unbound with unbindService()
@@ -104,7 +96,7 @@ public class MuldvarpService extends Service {
         // The service is no longer used and is being destroyed
     }
     
-    public InputStream getJSONData(String url){
+    private InputStream getJSONData(String url){
         DefaultHttpClient httpClient = new DefaultHttpClient();
         URI uri;
         InputStream data = null;
@@ -113,43 +105,46 @@ public class MuldvarpService extends Service {
             HttpGet method = new HttpGet(uri);
             HttpResponse response = httpClient.execute(method);
             data = response.getEntity().getContent();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            Logger.getLogger(MuldvarpService.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         return data;
     }
     
-    private class DownloadCourses extends AsyncTask<String, Void, ArrayList<Course>> {
-        ProgressDialog dialog;
+    private class DownloadCourses extends AsyncTask<String, Void, Boolean> {
         @Override
-//        protected void onPreExecute() {
-//            dialog = new ProgressDialog(CourseActivity.this);
-//            dialog.setMessage(getString(R.string.loading));
-//            dialog.setIndeterminate(true);
-//            dialog.setCancelable(false);
-//            dialog.show();
-//        }
-        
-        protected ArrayList<Course> doInBackground(String... urls) {
-            ArrayList<Course> items = new ArrayList<Course>();
+        protected Boolean doInBackground(String... urls) {
             try{
-                Reader json = new InputStreamReader(getJSONData(urls[0]));
-                Gson gson = new Gson();
-                SearchResults result = gson.fromJson(json, SearchResults.class);
-                items = (ArrayList<Course>)result.course;
+                cacheThis(
+                        new InputStreamReader(getJSONData(urls[0])), 
+                        new File(getCacheDir(), courseCache));
+                return true;
             }catch(Exception ex){
-                ex.printStackTrace();
-                dialog.setCancelable(true);
-                dialog.setMessage(getString(R.string.cannot_connect));
+                Logger.getLogger(MuldvarpService.class.getName()).log(Level.SEVERE, null, ex);
             }
-            return items;
+            return false;
         } 
         
         @Override
-        protected void onPostExecute(ArrayList<Course> items) {
-            courses = items;
-            dialog.dismiss();
+        protected void onPostExecute(Boolean retVal) {
+            // callback here
+        }
+    }
+    
+    private void cacheThis(Reader json, File f) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(f));
+            char[] buffer = new char[1024];
+            int length;
+            while((length = json.read(buffer)) != -1) {
+                writer.write(buffer, 0, length);
+            }
+            writer.flush();
+            writer.close();
+            json.close();
+        } catch (IOException ex) {
+            Logger.getLogger(MuldvarpService.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
