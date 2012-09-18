8a6bffaa06d3d3b373377d856c85b202355d5765
johan <johan@localhost>
2012-09-18 18:10:28 +0200

    Endringer til QuizActivity. Den har nå scrolling, og har forbedred layout. Forskjellige stiler kan prøves.

--- src/no/hials/muldvarp/v2/QuizActivity.java
+++ src/no/hials/muldvarp/v2/QuizActivity.java
@@ -9,10 +9,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import java.util.ArrayList;
 import java.util.List;
 import no.hials.muldvarp.R;
@@ -20,7 +17,7 @@ import no.hials.muldvarp.v2.domain.Alternative;
 import no.hials.muldvarp.v2.domain.Question;
 
 /**
- * This class defines an Activity used for Quiz-functionality.
+ * This class defines an Activity used for Quiz-functionality. 
  * 
  * @author johan
  */
@@ -42,32 +39,38 @@ public class QuizActivity extends Activity {
 
         makeTestData();
         
-        LinearLayout layout = new LinearLayout(this);
-        layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        layout.setOrientation(LinearLayout.HORIZONTAL);
+        //Set up layout inside a ScrollView
+        //Main Layout to hold the scrollview, somehow needed when doing this outside XML files
+        LinearLayout mainLayout = new LinearLayout(this);
+        //Set up scrollview and make sure it fills it's parent entirely
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
         
-        
+        //Loop through the questions in the array
         for(int i = 0; i < questions.size(); i++) {
-            LinearLayout question_layout = new LinearLayout(this);
-            question_layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+            
+            //Create TextView to hold question string
             TextView text = new TextView(this);
             text.setText(questions.get(i).getName());
-            layout.addView(text);
+            text.setTextSize(22);
+            linearLayout.addView(text);
             final Question question = questions.get(i);
             
-            LinearLayout alts = new LinearLayout(this);
+            //Loop through the alternatives in the array
             for(int k = 0; k < questions.get(i).getAlternatives().size(); k++) {
-                LinearLayout answ = new LinearLayout(this);
-                answ.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, 300));
-                answ.setOrientation(LinearLayout.HORIZONTAL);
-                
+                               
+                //Get alternatives and create a checkbox for each alternative with ID and text
                 Alternative alternative = questions.get(i).getAlternatives().get(k);
-                TextView quest = new TextView(this);
-                quest.setText(alternative.getName());
-                CheckBox cb = new CheckBox(this);
-                cb.setId(alternative.getId());
+                CheckBox checkBox = new CheckBox(this);
+                checkBox.setId(alternative.getId());
+                checkBox.setText(alternative.getName());
+                checkBox.setTextSize(16);
+                linearLayout.addView(checkBox);
                 
-                cb.setOnClickListener(new View.OnClickListener() {
+                checkBox.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
                         // Perform action on clicks, depending on whether it's now checked
                         if (((CheckBox) v).isChecked()) {
@@ -78,18 +81,14 @@ public class QuizActivity extends Activity {
                             }
                         }
                     }
-                });
+                }); //end onclick listener
                 
-                answ.addView(quest);
-                answ.addView(cb);
-                alts.addView(answ);
-            }
-            
-            question_layout.addView(alts);
-            layout.addView(question_layout);
-        }
+            } //end nested for loop            
+        } //end main for loop
         
-        setContentView(layout);
+        scrollView.addView(linearLayout);
+        mainLayout.addView(scrollView);
+        setContentView(mainLayout);
         
     }
     
@@ -104,24 +103,43 @@ public class QuizActivity extends Activity {
     
     public void makeTestData(){
         
-        List<Alternative> alt = new ArrayList<Alternative>();
-        Alternative a1 = new Alternative("HEEEEYAAAA");
-        a1.setId(1);
-        alt.add(a1);
-        Alternative a2 = new Alternative("HEEEEYAAAA");
-        a2.setId(2);
-        alt.add(a2);
-        Alternative a3 = new Alternative("murr");
-        a3.setId(3);
-        alt.add(a3);
-        Alternative a4 = new Alternative("derf");
-        a4.setId(4);
-        alt.add(a4);
-        Alternative a5 = new Alternative("Herf");
-        a5.setId(5);
-        alt.add(a5);
+//        List<Alternative> alt = new ArrayList<Alternative>();
+//        Alternative a1 = new Alternative("Svaralternativ 1");
+//        a1.setId(1);
+//        alt.add(a1);
+//        Alternative a2 = new Alternative("Svaralternativ 2");
+//        a2.setId(2);
+//        alt.add(a2);
+//        Alternative a3 = new Alternative("Svaralternativ 3");
+//        a3.setId(3);
+//        alt.add(a3);
+//        Alternative a4 = new Alternative("Svaralternativ 4");
+//        a4.setId(4);
+//        alt.add(a4);
+//        Alternative a5 = new Alternative("Svaralternativ 5");
+//        a5.setId(5);
+//        alt.add(a5);
+//        
+//        Question q = new Question("Hvilket svaralternativ er riktig?", alt, a2);
+//        questions.add(q);
+//        
+        for (int i = 1; i < 4; i++) {
+            
+            List<Alternative> alternatives = new ArrayList<Alternative>();
+            for (int n = 1; n < 6; n++) {
+                Alternative currentAlternative = new Alternative("Svaralternativ " + n);
+                currentAlternative.setId(i);
+                alternatives.add(currentAlternative);
+            }
+            
+            Alternative correctAnswer = new Alternative("Svaralternativ " + i);
+            
+            Question currentQuestion = new Question("Hvilket svaralternativ er riktig?", alternatives, correctAnswer);
+            questions.add(currentQuestion);
+            
+        }
         
-        Question q = new Question("Hvilket svaralternativ er riktig?", alt, a2);
-        questions.add(q);
+        
+        
     }
 }
